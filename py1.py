31df90b283c5e0dab358daa8583c3b2c257d1b75
Abdulrahman Aljoudi <aaljoudi@Abdulrahmans-MacBook-Pro.local>
2024-11-14 15:06:19 -0500

    excersize 1
    translate c++ code to python

--- py1.py
+++ py1.py
@@ -7,3 +7,16 @@ x = 3
 if x > 2:
     print ("Greater!")
     print ('That is all')
+
+# Excersize to translate c++ code to python
+
+x = 3
+
+if x > 2:
+    print ("Greater")
+    if x == 3:
+        print ("Equal")
+    else:
+        print ("Not equal")
+else:
+    print ("less")
