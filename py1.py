c7094dcf8eb61690b2145eebf4b6e977e15af973
Abdulrahman Aljoudi <aaljoudi@Abdulrahmans-MacBook-Pro.local>
2024-11-14 15:15:04 -0500

    jDoodle task 1

--- py1.py
+++ py1.py
@@ -20,3 +20,30 @@ if x > 2:
         print ("Not equal")
 else:
     print ("less")
+
+# J-Droid excersizes - task 1
+
+"""
+Task 1:
+
+Translate the following C++ code to Python
+int x = 3;
+int y = 4;
+
+if (x > y && !(x < y)) {
+  cout << "Equal";
+} else if (x > 100) {
+  cout << "Big";
+}
+
+"""
+
+# Task 1 - your solution here
+
+x = 3
+y = 4
+
+if x > y and not x < y:
+    print ("Equal")
+elif x > 100:
+    print ("Big")
