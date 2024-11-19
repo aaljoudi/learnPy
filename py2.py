4bdfd56dc0cb7e0052b3eaae8f3b1534b8f6392b
Abdulrahman Aljoudi <aaljoudi@Abdulrahmans-MacBook-Pro.local>
2024-11-19 15:23:37 -0500

    jDoodle excersize

--- py2.py
+++ py2.py
@@ -1,1 +1,20 @@
 print("Hello world")
+
+# Functions
+
+def add(x, y):
+    return x + y
+
+# J-Doodle excersize
+## This function does not work as intended
+def poly(x, coef):
+    sum = 0
+    for y in coef:
+        i = 0
+        sum += coef[i] * x**i
+        i += 1
+    
+    return sum
+
+coeffs = [4, 3, 1, 5]
+print (poly(2, coeffs))
